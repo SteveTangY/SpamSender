4b6b25cab261c9ddf4c0e6df16fd76e905ef6c63
SteveT <steve@sangfor.my>
2020-09-02 02:47:19 -0700

    save

--- mail_builder.py
+++ mail_builder.py
@@ -2,10 +2,15 @@
 # coding=utf-8
 
 from os import *
+from random import *
 from email.header import Header
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+
+def get_random_str()->str:
+    """ 获取一个由一个列表随机选择的字符串 """
+    return random.choice(["yooo", "cnds", "get_your_favourit", "index.", "home", "error"])
 
 class MailBuilder:
     """ 提供邮件构造功能 """
@@ -33,27 +38,45 @@ class MailBuilder:
         if not os.path.isfile(file_complete_path):
             print("file don't exist...\n")
             return False
-        tmp_list = file_name.split('.') # 对文件名切片
-        if tmp_list[len(tmp_list)-1] != "jpg" or tmp_list[len(tmp_list)-1] != "png":
-            print("file is not a jpg or png...\n")
+        f_name_list = file_name.split('.') # 对文件名切片
+        if f_name_list[len(f_name_list)-1] != "html":
+            print("file is not a html file...\n")
             return False
         with open(file_complete_path, 'r') as f: # 省略对文件大小判断排除bug
             html_content = f.read()
-            html_sub = MIMEText(html_content, 'html', 'utf-8')
-            html_sub.add_header("Content-Disposition", "attachment", filename=file_name)
-            self._mail.attach(html_sub)
+        html_sub = MIMEText(html_content, 'html', 'utf-8')
+        html_sub.add_header("Content-Disposition", "attachment", filename=file_name)
+        self._mail.attach(html_sub)
 
     def add_image(self, file_path:str, file_name:str):
         """ 添加图片附件, 失败返回False """
         # 省略了对参数的校验
         file_complete_path = file_path / file_name
-        if not os.paht.isfile(file_complete_path):
-            print("file don't exitst...\n")
+        if not os.path.isfile(file_complete_path):
+            print("file don't exist...\n")
             return False
+        f_name_list = file_name.split('.') # 对文件名切片
+        if f_name_list[len(f_name_list)-1] != "jpg" or f_name_list[len(f_name_list)-1] != "png":
+            print("file is not a jpg or png...\n")
+            return False
+        with open(file_complete_path, 'r') as f:
+            image = f.read() # 省略对文件大小的检查
+        image_sub = MIMEImage(image)
+        image_sub.add_header("Content-ID", get_random_str())
+        image_sub.add_header("Content-Disposition", "attachment", filename=file_name)
+        self._mail.attach(image_sub)
 
     def add_file(self, file_path:str, file_name:str):
         """ 添加文件附件 """
-        pass
+        # 省略了对参数的校验
+        file_complete_path = file_path / file_name
+        if not os.path.isfile(file_complete_path):
+            print("file don't exist...\n")
+            return False
+        with open(file_complete_path) as f:
+            txt = f.read()
+        
+
 
     def get_mail(self):
         """ 获取当前构建的MIME邮件对象，失败返回None """
